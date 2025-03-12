cf445cfab644677c793d861d6f47dcff4c1c9ea1
kuewu pamela <kuewupamela@gmail.com>
2025-03-12 09:31:24 +0000

    update

---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const onboarding = () => {
+  return(
+    <view>
+      <text>Onboarding</text>
+    </view>
+  )
+}
+export default onboarding
+const styles = StyleSheet.create({})
