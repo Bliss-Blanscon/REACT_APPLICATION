924bb43c33a0f304e5f8d7c72beb5c0eb8be5242
kuewu pamela <kuewupamela@gmail.com>
2025-03-20 10:42:18 +0000

    update

---
+++ Mobileapp/app/Games/[id].tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const gamesDetails = () => {
+  return (
+    <View>
+      <Text>gamesDetails</Text>
+    </View>
+  )
+}
+
+export default gamesDetails
+
+const styles = StyleSheet.create({})
