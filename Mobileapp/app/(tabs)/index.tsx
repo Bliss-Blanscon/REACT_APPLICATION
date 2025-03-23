4a8102f3a6b4c4225d8e2c92937138e46500d4ce
kuewu pamela <kuewupamela@gmail.com>
2025-03-23 18:54:14 +0000

    update

--- Mobileapp/app/(tabs)/index.tsx
+++ Mobileapp/app/(tabs)/index.tsx
@@ -1,9 +1,70 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+const person = require("@/assets/images/Profile Image.png")
 
-export default function Index() {
+const index = () => {
   return (
-    <View>
-      <Text>Hello Everyone</Text>
+    <View style={styles.container}>
+
+
+
+     {/* header view */}
+     <View style={styles.firstView}>
+
+
+      <Text style={styles.devs}>Hello , Devs</Text>
+      <Text style={styles.task}>14 tasks today</Text>
+
+<TouchableOpacity style={styles.iconperson}>
+      <Image source={person} />
+      </TouchableOpacity>
+     </View>
     </View>
-  );
+  )
 }
+
+export default index
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#F7F0E8',
+    width:393,
+    height:852,
+    borderRadius: 30
+  },
+  devs:{
+fontFamily: 'Lato',
+fontWeight: 700,
+fontSize: 32,
+
+
+letterSpacing:0,
+color:'#000000'
+  },
+  task:{
+    fontFamily: 'lato',
+    fontWeight: 500,
+    fontSize: 12,
+  },
+  firstView:{
+    width:354,
+    height:52,
+    verticalAlign:'top',
+  paddingTop:52,
+  paddingBottom:30,
+  paddingLeft:20,
+  paddingRight:19
+
+  },
+  iconperson:{
+    width:46,
+    height:45,
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft:306,
+verticalAlign: 'top',
+marginTop:-40
+  }
+  
+})
--- myFoodApp/app/index.tsx
+++ myFoodApp/app/index.tsx
@@ -1,13 +1,31 @@
-import {StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {StyleSheet, Text, TouchableOpacity, ScrollView,View, Image, ImageBackground } from "react-native";
+const logoImg = require("@/assets/images/adaptive-icon.png"); 
+
+
 
 export default function Index() {
  return (
-     <View style={styles.container}>
-       <Text>Mama</Text>
-    
-     <TouchableOpacity style={styles.btn}>
-        <Text>Sign Up</Text>
-     </TouchableOpacity>
+     <View style={{flex:1, backgroundColor: "plum", padding: 60}}>
+      <ScrollView>
+       {/* <Text><Text style={{color:"white"}}>Hello </Text>World</Text> */}
+<Image source={logoImg} style={{width: 300, height: 300}}/>
+<Text>
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat consectetur ex minima, similique voluptatum voluptatem at, quae laborum esse harum eum amet obcaecati, exercitationem explicabo architecto illo totam magnam deserunt?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium molestiae dolorum molestias quo, quaerat rem maiores itaque dolores culpa libero. Natus sit incidunt aliquid dolores vel consectetur ea dicta sapiente.
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, facilis pariatur? Facere, ullam soluta asperiores amet tempora aspernatur accusamus distinctio dolorum quisquam quo repudiandae veritatis perferendis tempore numquam natus eius.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. In, obcaecati optio. Harum, aperiam! Eos cupiditate earum nisi eaque alias omnis rerum, nesciunt, nam et quia maiores, blanditiis adipisci animi molestiae.
+</Text>
+
+<Image source={logoImg} style={{width: 300, height: 300}}/>
+
+
+
+{/* <Image source={{ uri:'https://picsum.photos/300'}} style={{width: 300, height: 300}}/> */} 
+{/* <ImageBackground source={logoImg} style={{flex: 1}}>
+<Text>Image background</Text>
+</ImageBackground> */}
+</ScrollView>
+
      </View>
    )
   
@@ -15,17 +33,5 @@ export default function Index() {
  
  
  
- const styles = StyleSheet.create({
-   container:{
-    flex: 1,
-     backgroundColor:'green',
-     justifyContent:'center',
-     alignItems: 'center',
-     
-   },
-   btn:{
-display: 'flex',
-
-   }
- })
+ const styles = StyleSheet.create({})
  
